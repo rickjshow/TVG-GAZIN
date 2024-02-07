b7415d99975e3750b9d4d3d7b705dd9c5dea52a9
Henrique Bertaggi <henriquebertaggi@gmail.com>
2024-02-06 22:04:42 -0300

    tela de exclusao, desativar facilitadores

--- excluiSessao.php
+++ excluiSessao.php
@@ -12,13 +12,17 @@ if(isset($_GET['idGS'])) {
     $delete1->bindValue(":id", $idSessao);
 
     if($delete1->execute()){
+
+        $querySituacao = "UPDATE usuarios SET situacao = 'Inativo' WHERE permission = 'limited'";
+        $stmt2 = $pdo->prepare( $querySituacao);
+        $stmt2->execute();
+
         session_start();
         $_SESSION['alerta'] = array('tipo' => 'success', 'mensagem' => 'Sessão excluida com sucesso!');
         header("location: novaEdicao.php");
         exit();
     }
     
-} 
-
+}
 
 ?>
